2bd900718dbc2a9051f2d63ed0c5eff27e18c849
Mihai Bojescu <bojescu.mihai@gmail.com>
2024-01-14 00:24:51 +0200

    Add basic convolutional discriminator

--- src/nn/discriminator/model.py
+++ src/nn/discriminator/model.py
@@ -2,8 +2,34 @@ import torch
 
 
 class Discriminator(torch.nn.Module):
-    def __init__(self):
-        pass
+    __device: torch.device
+
+    def __init__(self, device: torch.device, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.__device = device
+
+        self.__conv_1 = torch.nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1, device=device)
+        self.__batch_norm_1 = torch.nn.BatchNorm2d(32, device=device)
+        self.__conv_2 = torch.nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1, device=device)
+        self.__batch_norm_2 = torch.nn.BatchNorm2d(64, device=device)
+        self.__conv_3 = torch.nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1, device=device)
+        self.__batch_norm_3 = torch.nn.BatchNorm2d(128, device=device)
+        self.__conv_4 = torch.nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1, device=device)
+        self.__batch_norm_4 = torch.nn.BatchNorm2d(256, device=device)
+        self.__conv_5 = torch.nn.Conv2d(256, 1, kernel_size=2, stride=1, padding=0, device=device)
+
+        self.__activation_function = torch.nn.ReLU()
+        self.__output_activation_function = torch.nn.Sigmoid()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = x.to(self.__device)
+
+        x = self.__activation_function(self.__batch_norm_1(self.__conv_1(x)))
+        x = self.__activation_function(self.__batch_norm_2(self.__conv_2(x)))
+        x = self.__activation_function(self.__batch_norm_3(self.__conv_3(x)))
+        x = self.__activation_function(self.__batch_norm_4(self.__conv_4(x)))
+        x = self.__output_activation_function(self.__conv_5(x))
+        x = x[:, :, 0, 0]
+
         return x
