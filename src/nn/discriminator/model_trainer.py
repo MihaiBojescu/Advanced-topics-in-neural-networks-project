af523775e5ed546b0536cd7784724d6a7431555d
Mihai Bojescu <bojescu.mihai@gmail.com>
2024-01-15 00:42:17 +0200

    Update discriminator trainer to run only on 1 instance from a dataset instead of the whole dataset

--- src/nn/discriminator/model_trainer.py
+++ src/nn/discriminator/model_trainer.py
@@ -33,37 +33,29 @@ class DiscriminatorTrainer:
 
         self.__loss_function = self.__loss_function.to(device=self.__device, non_blocking=self.__device == "cuda")
 
-    def run(
-        self,
-        batched_dataloader: DataLoader,
-    ) -> torch.Tensor:
+    def run(self, real_image: torch.Tensor) -> torch.Tensor:
         self.__discriminator.train()
         self.__generator.eval()
-        training_loss = torch.Tensor([0], device=self.__device)
 
-        for entry in batched_dataloader:
-            real_image, _ = entry
-            fake_image = self.__generator(x=torch.rand(real_image.shape))
-            real_image_target = torch.zeros((real_image.shape[0], 1)).fill_(0.85)
-            fake_image_target = torch.zeros((real_image.shape[0], 1)).fill_(0.00)
+        fake_image = self.__generator(x=torch.rand(real_image.shape))
+        real_image_target = torch.zeros((real_image.shape[0], 1)).fill_(0.85)
+        fake_image_target = torch.zeros((real_image.shape[0], 1)).fill_(0.00)
 
-            real_image = real_image.to(device=self.__device, non_blocking=self.__device == "cuda")
-            fake_image = fake_image.to(device=self.__device, non_blocking=self.__device == "cuda")
-            real_image_target = real_image_target.to(device=self.__device, non_blocking=self.__device == "cuda")
-            fake_image_target = fake_image_target.to(device=self.__device, non_blocking=self.__device == "cuda")
+        real_image = real_image.to(device=self.__device, non_blocking=self.__device == "cuda")
+        fake_image = fake_image.to(device=self.__device, non_blocking=self.__device == "cuda")
+        real_image_target = real_image_target.to(device=self.__device, non_blocking=self.__device == "cuda")
+        fake_image_target = fake_image_target.to(device=self.__device, non_blocking=self.__device == "cuda")
 
-            self.__optimiser.zero_grad()
-            real_image_result = self.__discriminator(x=real_image)
-            fake_image_result = self.__discriminator(x=fake_image)
-            loss = self.__loss_function(real_image_result, real_image_target) + self.__loss_function(
-                fake_image_result, fake_image_target
-            )
-            loss.backward()
-            self.__optimiser.step()
+        self.__optimiser.zero_grad()
+        real_image_result = self.__discriminator(x=real_image)
+        fake_image_result = self.__discriminator(x=fake_image)
+        loss = self.__loss_function(fake_image_result, fake_image_target) - self.__loss_function(
+            real_image_result, real_image_target
+        )
+        loss.backward()
+        self.__optimiser.step()
 
-            training_loss += loss
-
-        return training_loss
+        return loss
 
     def export(self) -> None:
         torch.save(self.__discriminator.state_dict(), f"{self.__exports_path}/discriminator_{time.time_ns()}.pt")
