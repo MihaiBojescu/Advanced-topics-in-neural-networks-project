afb71a81116e4d3bd7101df7c024f62060b6224a
Mihai Bojescu <bojescu.mihai@gmail.com>
2024-01-14 16:01:20 +0200

    Fix 1 bug in unet, make unet not use biases

--- src/nn/generator/model.py
+++ src/nn/generator/model.py
@@ -11,40 +11,40 @@ class Generator(nn.Module):
 
         self.__device = device
 
-        self.encoder_1_1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
-        self.encoder_1_2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.encoder_1_1 = nn.Conv2d(3, 64, kernel_size=3, padding=1, bias=False)
+        self.encoder_1_2 = nn.Conv2d(64, 64, kernel_size=3, padding=1, bias=False)
         self.pool_1 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.encoder_2_1 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.encoder_2_2 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.encoder_2_1 = nn.Conv2d(64, 128, kernel_size=3, padding=1, bias=False)
+        self.encoder_2_2 = nn.Conv2d(128, 128, kernel_size=3, padding=1, bias=False)
         self.pool_2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.encoder_3_1 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
-        self.encoder_3_2 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.encoder_3_1 = nn.Conv2d(128, 256, kernel_size=3, padding=1, bias=False)
+        self.encoder_3_2 = nn.Conv2d(256, 256, kernel_size=3, padding=1, bias=False)
         self.pool_3 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.encoder_4_1 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
-        self.encoder_4_2 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.encoder_4_1 = nn.Conv2d(256, 512, kernel_size=3, padding=1, bias=False)
+        self.encoder_4_2 = nn.Conv2d(512, 512, kernel_size=3, padding=1, bias=False)
         self.pool_4 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.encoder_5_1 = nn.Conv2d(512, 1024, kernel_size=3, padding=1)
-        self.encoder_5_2 = nn.Conv2d(1024, 1024, kernel_size=3, padding=1)
+        self.encoder_5_1 = nn.Conv2d(512, 1024, kernel_size=3, padding=1, bias=False)
+        self.encoder_5_2 = nn.Conv2d(1024, 1024, kernel_size=3, padding=1, bias=False)
 
         self.upconv_1 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
-        self.decoder_1_1 = nn.Conv2d(1024, 512, kernel_size=3, padding=1)
-        self.decoder_1_2 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.decoder_1_1 = nn.Conv2d(1024, 512, kernel_size=3, padding=1, bias=False)
+        self.decoder_1_2 = nn.Conv2d(512, 512, kernel_size=3, padding=1, bias=False)
 
         self.upconv_2 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.decoder_2_1 = nn.Conv2d(512, 256, kernel_size=3, padding=1)
-        self.decoder_2_2 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.decoder_2_1 = nn.Conv2d(512, 256, kernel_size=3, padding=1, bias=False)
+        self.decoder_2_2 = nn.Conv2d(256, 256, kernel_size=3, padding=1, bias=False)
 
         self.upconv_3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.decoder_3_1 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
-        self.decoder_3_2 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.decoder_3_1 = nn.Conv2d(256, 128, kernel_size=3, padding=1, bias=False)
+        self.decoder_3_2 = nn.Conv2d(128, 128, kernel_size=3, padding=1, bias=False)
 
         self.upconv_4 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.decoder_4_1 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
-        self.decoder_4_2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.decoder_4_1 = nn.Conv2d(128, 64, kernel_size=3, padding=1, bias=False)
+        self.decoder_4_2 = nn.Conv2d(64, 64, kernel_size=3, padding=1, bias=False)
 
         self.outconv = nn.Conv2d(64, 3, kernel_size=1)
         self.activation_function = nn.Sigmoid()
@@ -73,7 +73,6 @@ class Generator(nn.Module):
 
         x = self.activation_function(self.encoder_5_1(self.pool_4(x)))
         x = self.activation_function(self.encoder_5_2(x))
-        x_encoded_list.append(x)
 
         x_encoded = x_encoded_list.pop()
         x = self.upconv_1(x)
