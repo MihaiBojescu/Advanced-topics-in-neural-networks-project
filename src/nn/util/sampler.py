6b407363321ca43455506acc410ea5478bae7555
Mihai Bojescu <bojescu.mihai@gmail.com>
2024-01-16 02:27:08 +0200

    Update how sampler works

--- src/nn/util/sampler.py
+++ src/nn/util/sampler.py
@@ -2,17 +2,20 @@ import os
 import time
 import typing as t
 import torch
+from torchvision.transforms.v2 import ToPILImage
 from matplotlib import pyplot
 
 
 class Sampler:
     __good_sample_threshold: float
     __samples_path: str
+    __transform: ToPILImage
 
     def __init__(self, good_sample_threshold: float, samples_path: str) -> None:
         self.__good_sample_threshold = good_sample_threshold
         self.__samples_path = samples_path
         self.__create_samples_path(samples_path)
+        self.__transform = ToPILImage()
 
     def __create_samples_path(self, samples_path: str) -> None:
         if os.path.exists(samples_path):
@@ -32,23 +35,23 @@ class Sampler:
         ):
             figure, subplots = pyplot.subplots(1, 3)
 
-            noise_sample = noise_sample.permute(1, 2, 0)
-            generator_sample = generator_sample.permute(1, 2, 0)
-            discriminator_sample_image = torch.zeros(noise_sample.shape)
+            noise_sample_image = self.__transform(noise_sample)
+            generator_sample_image = self.__transform(generator_sample)
+            discriminator_sample_image = torch.zeros(noise_sample.shape).permute(1, 2, 0)
             discriminator_sample_image[:, :] = torch.Tensor(
                 [0.0, 1.0, 0.0] if discriminator_sample.item() > self.__good_sample_threshold else [1.0, 0.0, 0.0]
             )
 
             figure.tight_layout(pad=2.0)
-            subplots[0].imshow(noise_sample.detach().numpy())
+            subplots[0].imshow(noise_sample_image)
             subplots[0].set_title("Generator seed")
             subplots[0].set_xlabel("Width")
             subplots[0].set_ylabel("Height")
-            subplots[1].imshow(generator_sample.detach().numpy())
+            subplots[1].imshow(generator_sample_image)
             subplots[1].set_title("Generator output")
             subplots[1].set_xlabel("Width")
             subplots[1].set_ylabel("Height")
-            subplots[2].imshow(discriminator_sample_image.detach().numpy())
+            subplots[2].imshow(discriminator_sample_image)
             subplots[2].set_title("Discriminator output")
             subplots[2].axis("off")
 
